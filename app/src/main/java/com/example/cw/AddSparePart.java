7090037a85ad7cef61d6c1cc45aee4cf02528353
=Ghosty098 <=jad.samaan15@gmail.com>
2023-12-06 23:31:18 +0800

    Implemented intents for the back arrow button

--- app/src/main/java/com/example/cw/AddSparePart.java
+++ app/src/main/java/com/example/cw/AddSparePart.java
@@ -2,13 +2,27 @@ package com.example.cw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddSparePart extends AppCompatActivity {
+
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_spare_part);
+        btnBack = findViewById(R.id.back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), AdminPage.class);
+                    startActivity(intent);
+                    finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cw/EditSparePart.java
+++ app/src/main/java/com/example/cw/EditSparePart.java
@@ -2,13 +2,26 @@ package com.example.cw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditSparePart extends AppCompatActivity {
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_spare_part);
+        btnBack = findViewById(R.id.back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AdminPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
